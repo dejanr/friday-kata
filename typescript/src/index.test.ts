2ad46dea19780649386670d8dbe435ab32f3b071
dejanr <dejan@ranisavljevic.com>
2024-06-21 11:40:02 +0200

    Format code

--- typescript/src/index.test.ts
+++ typescript/src/index.test.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-describe('Index', async () => {
-  it('is thruthy', async () => {
+describe("Index", async () => {
+  it("is thruthy", async () => {
     expect(true).toBeTruthy();
   });
 });
